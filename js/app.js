e4e23809ffdf406b18d3611d39b8eefb89467cb8
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-29 16:30:26 +0200

    add-display-grid

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
-
+//definiamo il numero di celle
 let gridSize = 10;
 let cellNumber = gridSize ** 2;
-const gridContainer = document.querySelector('.container');
 
+//richiamiamo il container
+const gridContainer = document.querySelector('.container');
+//richiamiamo il button
 const playButton = document.getElementById('play_button')
 
 playButton.addEventListener('click', function () { 
     playButton.style.display = 'none';
-    gridContainer.style.display = 'block';
+    gridContainer.style.display = 'grid';
     console.log(gridContainer);
 })
 
-
+//generiamo e appendiamo al container le nostre celle
 for ( let i = 0; i < cellNumber; i++) {
     const cell = getSquareElement();
     gridContainer.append(cell);
