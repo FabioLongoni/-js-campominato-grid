849789c77565f32a3990aa5ce8861457d49cc761
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-29 15:04:57 +0200

    add-function-Square-Element

--- js/app.js
+++ js/app.js
@@ -5,7 +5,14 @@ const gridContainer = document.querySelector('.container');
 
 
 for ( let i = 0; i < cellNumber; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('square');
+    const cell = getSquareElement();
     gridContainer.append(cell);
+    cell.innerHTML = i + 1;
+}
+
+function getSquareElement() {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    console.log(square);
+    return square;
 }
